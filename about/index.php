7891eec0c4a3bb28c704f350dd1707dee7f5c1ad
Adrial Armijo <adrial.armijo@csu.fullerton.edu>
2018-08-31 23:42:08 -0700

    syncing to server

---
+++ about/index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="normalize.css" />
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script> 
+		<title>MIM - Resume</title>
+	</head>
+	<body lang="en">
+		<section id="main">
+		<header id="title">
+			<h1>Adrial Armijo</h1>
+			<span class="subtitle">Enthusiastic upcoming graduate pursuing a Graphics Programmer position to utilize my strong problem-solving and 3D programming skills for a mutually beneficial industry experience</span>
+		</header>
+		<section class="main-block">
+			<h2>Experience</h2>
+			<section class="blocks">
+				<div class="date">
+					<span>present</span><span>May 2018</span>
+				</div>
+				<div class="decorator"></div>
+				<div class="details">
+				<header>
+					<h3>Information Technology Student Assistant</h3>
+					<span class="place">CSU Fullerton Auxiliary Services Corp.</span>
+					<span class="location">Fullerton, CA</span>
+				</header>
+				<div>
+				<ul>
+					<li>Get, compile, and analyze data from enterprise resource planning service, NetSuite</li>
+					<li>Assemble and distribute legacy system data for migration</li>
+					<li>Attend training conference for cloud-based data integration system, Dell Boomi</li>
+					<li>Setup, monitor, and maintain point of sale terminals: MBS, Micros, Veriphone and NetSuite</li>
+					<li>Initiate company-wide Windows desktop software updates and back-ups</li>
+					<li>Investigate and troubleshoot issues with office staff printer, desktop PC, and audio/video equipment</li>
+					<li>Maintain and monitor self-guided progress on individually assigned projects</li>
+					<li>Participate in assisting IT Director on a daily basis</li>
+				</ul>
+				</div>
+				</div>
+			</section>
+			<section class="blocks">
+				<div class="date">
+					<span>May 2018</span><span>Nov 2016</span>
+				</div>
+				<div class="decorator"></div>
+				<div class="details">
+				<header>
+					<h3>General Merchandise Supervisor, Service Center</h3>
+					<span class="place">CSU Fullerton Auxiliary Services Corp.</span>
+					<span class="location">Fullerton, CA</span>
+				</header>
+				<div>
+				<ul>
+					<li>Lead a small team for division operations including computer, phone, and tablet repairs</li>
+					<li>Ensure performance in software and hardware repairs, turnaround times, and customer service</li>
+					<li>Get, compile, and analyze fiscal reports, invoices, and performance reports to upper management</li>
+					<li>Train team in new process and procedures established and designed by myself</li>
+					<li>Assign and delegate repairs and daily needs to employees</li>
+					<li>Plan and ensure employee availability based on forcast budget</li>
+					<li>Interview and hire new employees as needed</li>
+					<li>Held increasingly responsible positions:</li>
+					<ul>
+						<li>Cashier Associate, Tech Sales Associate, Technician, Lead Technician
+					</ul>
+				</ul>
+				</div>
+				</div>
+			</section>
+		</section>
+		<section class="main-block">
+			<h2>Non-academic Projects</h2>
+			<section class="blocks">
+			<div class="date"><span>present</span><span>May 2018</span></div>
+			<div class="decorator"></div>
+			<div class="details">
+				<header>
+				<h3>Assisting Server Maintenence</h3>
+					<span class="place">pt23.net</span>
+					<span class="location">Fullerton, CA</span>				
+				</header>
+			<div>
+			<ul>
+				<li>Work with group version control through gitLab</li>
+				<li>Maintain Ubuntu server software</li>
+				<li>Active full-stack web-development and maintenance</li>
+			</ul>
+			</div>
+			</div>
+			</section>
+		</section>
+		<section class="main-block concise">
+		<h2>Education</h2>
+		<section class="blocks">
+			<div class="date"><span>Fall 2018</span><span>Fall 2013</span></div>
+			<div class="decorator"></div>
+			<div class="details">
+			<header>
+				<h3>Bachelor of Science, Computer Science <i>emphisis on Media and Game Design</i></h3>
+				<span class="place">California State University Fullerton</span>
+				<span class="location">Fullerton, CA</span>
+			</header>
+			<div>
+			<ul>
+				<li>Relevant electives:</li>
+				<ul>
+					<li>CPSC 489 Game Development Project (A), CPSC 486 Game Programming (B), CPSC 484 Principles of Computer Graphics (B), CPSC 386 Intro to Game Design (C)</li>
+				</ul>
+				<li>Relevant projects:</li>
+				<ul>
+					<li>"Stinky Shoe Clicker" - self-orignated game design, programmed and implemented by myself and small team in python-pygame Geany Ubuntu</li>
+					<li>"Ball Maze" - self-originated game design, programmed and implemented by myself and one other in C++ using Unreal Engine Windows/Mac and Visual Studios/Xcode</li>
+					<li>OpenGL Vertex Shader - class project in C++/OpenGL using Geany Ubuntu Matte</li>
+					<li>OpenGL Camera - class project in C++/OpenGL using Geany Ubuntu Matte</li>
+				</ul>
+				<li>Projects were submitted using a private student/class github repository, but final copies are listed on http://github.com/adrialarmijo</li>
+				</ul>
+			</ul>
+			</div>
+			</div>
+		</section>
+		</section>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Make. Inspire. Motivate. - MIM</title>
+	<link href="style.css" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light"> 
+	<a class="navbar-brand" href="">MIM</a>
+		<ul class="navbar-nav">
+			<li class="nav-item active">
+				<a class="nav-link" href="/">Home<span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="/projects" id="navbarDropDownMenuProjects" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Projects</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuProjects">
+					<a class="dropdown-item" href="#">Game Programming</a>
+					<a class="dropdown-item" href="#">Algorithms & Artificial Intelligence</a>
+					<a class="dropdown-item" href="#">Database & Networking</a>
+				</div>
+			</li>
+			<li>
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropDownMenuAboutMe" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About Me</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuAboutMe">
+					<a class="dropdown-item" href="/about">Resume</a>
+					<a class="dropdown-item" href="#">Interests</a>
+				</div>
+			</li>
+		</ul>
+</nav>
+
+<div class="container" style="margin-top:50px">
+	<h1>Adrial Armijo | Software Developer & Information Technology</h1>	
+	<p>I'm <b>Adrial Armijo</b>, born to <b> make, motivate, and inspire</b> through programming</p>
+	<div class="container">
+	<h2>How I can help you</h2>
+		<div class="row">
+			<div class="col-sm-4">
+			<h3>Game Programming</h3>
+			<p>My degree emphasis focused on media and graphics programming.
+			I have experience in the OpenGL rendering library.</p> 
+			</div>
+			<div class="col-sm-4">
+			<h3>Algorithms & Artificial Intelligence</h3>
+			<p>I have completed various exercises to solve algorithmic problems
+			I have made simple game AIs for competing against.</p> 
+			</div>
+			<div class="col-sm-4">
+			<h3>Database & Networking</h3>
+			<p>I have Information Technology work experience in database management
+			I continue to help maintain and assist this site's server and network</p> 
+			</div>
+		</div>
+	</div>
+</div>
+<div id="footer">
+	<p>&copy; Copyright MIM 2018</p>
+</div>
+</body>
+</html>
